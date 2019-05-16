af9a2314575472273191c3d1480e704018d81020
German <germanr_g@hotmail.com>
2019-05-16 16:31:19 -0500

    version 18.4

--- src/app/components/coments/coments.component.ts
+++ src/app/components/coments/coments.component.ts
@@ -1,5 +1,5 @@
 import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-coments',
@@ -12,5 +12,8 @@ export class ComentsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+ 
+
 
 }
